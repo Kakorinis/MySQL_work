ac5b3c786aea9a6f062f315d0cb99d106eba9f55
Kakorinis <kakorin-faugi@mail.ru>
2021-08-15 23:36:31 +0000

    Kakorin 2 lesson

---
+++ Script.sql
@@ -0,0 +1,23 @@
+
+CREATE DATABASE users;
+
+USE users;
+
+CREATE TABLE users(
+id INT NOT NULL,
+Name VARCHAR(20),
+PRIMARY KEY(id)
+);
+
+INSERT INTO users(id, Name)  
+VALUES (1, 'Kakorin Ilya');
+
+INSERT INTO users(id, Name)  
+VALUES (2, 'Orlov Denis');
+
+CREATE DATABASE sample;
+
+
+
+
+
